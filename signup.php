7c497d273b90ffae9ab11475bad812e3ccbd9055
Blissmode <krithickrockz@gmail.com>
2018-06-25 22:31:35 +0530

    Setup the form to send POST request to the server

---
+++ signup.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+You chose to work with the username <?php echo $_POST["username"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?> <br>
+Your password is <?php echo $_POST["password"]; ?><br>
+You repeated the password as <?php echo $_POST["repassword"]; ?><br>
+</body>
+</html>
